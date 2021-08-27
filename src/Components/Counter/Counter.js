da24aeb34c771c7941ce070b17834113045caa43
DemonRed <demonred8@gmail.com>
2021-08-27 21:10:36 +0300

    added count step function

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  let [countStep, setCountStep] = useState(1);
   const [intervalID, setIntervalID] = useState(0);
   let [intervalMS, setIntervalMS] = useState(1000);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  const increment = () => setCount(count + countStep);
+  const decrement = () => setCount(count - countStep);
 
   const incrementMS = () => {
     setIntervalMS((intervalMS += 1000));
     if (intervalID) {
       clearInterval(intervalID);
       const newIntervalID = setInterval(() => {
-        setCount((count) => count + 1);
+        setCount((count) => count + countStep);
       }, intervalMS);
       setIntervalID(newIntervalID);
     }
@@ -27,7 +28,7 @@ function Counter() {
     if (intervalID) {
       clearInterval(intervalID);
       const newIntervalID = setInterval(() => {
-        setCount((count) => count + 1);
+        setCount((count) => count + countStep);
       }, intervalMS);
       setIntervalID(newIntervalID);
     }
@@ -38,26 +39,52 @@ function Counter() {
       clearInterval(intervalID);
       setIntervalID(0);
       setIntervalMS(1000);
+      setCountStep(1);
       setCount(0);
       event.target.textContent = "Start interval";
     } else {
       const newIntervalID = setInterval(() => {
-        setCount((count) => count + 1);
+        setCount((count) => count + countStep);
       }, intervalMS);
       setIntervalID(newIntervalID);
       event.target.textContent = "Stop interval";
     }
   };
 
+  const incrementStep = () => {
+    setCountStep((countStep += 10));
+    if (intervalID) {
+      clearInterval(intervalID);
+      const newIntervalID = setInterval(() => {
+        setCount((count) => count + countStep);
+      }, intervalMS);
+      setIntervalID(newIntervalID);
+    }
+  };
+
+  const decrementStep = () => {
+    setCountStep((countStep -= 10));
+    if (intervalID) {
+      clearInterval(intervalID);
+      const newIntervalID = setInterval(() => {
+        setCount((count) => count + countStep);
+      }, intervalMS);
+      setIntervalID(newIntervalID);
+    }
+  };
+
   return (
     <div>
       <h1>{count}</h1>
-      <h2>{intervalMS} Interval MS</h2>
+      <h2>{countStep}</h2>
+      <h3>{intervalMS} Interval MS</h3>
       <button onClick={interval}>Start interval</button>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={incrementMS}>Interval +</button>
-      <button onClick={decrementMS}>Interval -</button>
+      <button onClick={increment}>Increment counter</button>
+      <button onClick={decrement}>Decrement counter</button>
+      <button onClick={incrementStep}>Increment counter Step by 10</button>
+      <button onClick={decrementStep}>Decrement counter Step by 10</button>
+      <button onClick={incrementMS}>Interval MS +</button>
+      <button onClick={decrementMS}>Interval MS -</button>
     </div>
   );
 }
